e3481319662bd6624a3e2d3c69bbe7fd6e59dc7e
Jeromy <jeromyj@gmail.com>
2018-02-21 16:31:39 -0800

    make implementing the notifiee interface easier

---
+++ notifiee.go
@@ -0,0 +1,54 @@
+package net
+
+import (
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+type NotifyBundle struct {
+	ListenF      func(Network, ma.Multiaddr)
+	ListenCloseF func(Network, ma.Multiaddr)
+
+	ConnectedF    func(Network, Conn)
+	DisconnectedF func(Network, Conn)
+
+	OpenedStreamF func(Network, Stream)
+	ClosedStreamF func(Network, Stream)
+}
+
+var _ Notifiee = (*NotifyBundle)(nil)
+
+func (nb *NotifyBundle) Listen(n Network, a ma.Multiaddr) {
+	if nb.ListenF != nil {
+		nb.ListenF(n, a)
+	}
+}
+
+func (nb *NotifyBundle) ListenClose(n Network, a ma.Multiaddr) {
+	if nb.ListenCloseF != nil {
+		nb.ListenCloseF(n, a)
+	}
+}
+
+func (nb *NotifyBundle) Connected(n Network, c Conn) {
+	if nb.ConnectedF != nil {
+		nb.ConnectedF(n, c)
+	}
+}
+
+func (nb *NotifyBundle) Disconnected(n Network, c Conn) {
+	if nb.DisconnectedF != nil {
+		nb.DisconnectedF(n, c)
+	}
+}
+
+func (nb *NotifyBundle) OpenedStream(n Network, s Stream) {
+	if nb.OpenedStreamF != nil {
+		nb.OpenedStreamF(n, s)
+	}
+}
+
+func (nb *NotifyBundle) ClosedStream(n Network, s Stream) {
+	if nb.ClosedStreamF != nil {
+		nb.ClosedStreamF(n, s)
+	}
+}
